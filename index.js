97a7bccc4a98896b4881e18fd292d5b19f8d68ba
Erfan Riahi <erfanriahi90@gmail.com>
2023-01-23 00:07:23 +0330

    fix index page

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+const header = document.querySelector("header");
 const bars = document.querySelector(".bars");
+const rightSide = document.querySelector(".rightSide");
 const sidebarMenu = document.querySelector("#sidebar-menu ul");
 const main = document.querySelector("main");
 let flag = false;
+
+let userName = sessionStorage.getItem("userName");
+
+if (!userName) {
+  rightSide.innerHTML = `<a href="sign-up.html" class="links">Sign up</a><span> /
+                    </span><a href="src/login.html" class="links login">Login</a>`;
+  bars.insertAdjacentElement(
+    "afterend",
+    `<a href="src/sign-up.html" class="links signUp-btn">Sign up</a>`
+  );
+} else {
+  const a = document.createElement("a");
+  a.href = "src/profile.html";
+  a.className = "links signUp-btn";
+  a.innerHTML = `<i class="fa-regular fa-user"></i> ${userName}`;
+  rightSide.innerHTML = `<a href="src/profile.html" class="links"><i class="fa-regular fa-user"></i> ${userName}</a>`;
+  bars.insertAdjacentElement("afterend", a);
+}
 
 bars.addEventListener("click", () => {
   if (!flag) {
